63cb1b1c3b67bd0bb0b08a5a1531ae80eeb4d3a8
David Martinez <tinezillo_94@hotmail.com>
2021-09-13 12:42:07 +0200

    Routes api write

--- Project-Pink-Panther/routes/index.js
+++ Project-Pink-Panther/routes/index.js
@@ -3,6 +3,6 @@ module.exports = app => {
     app.use("/", require("./base.routes"))
     app.use("/", require("./auth.routes"))
     app.use("/upload", require("./upload.routes"))
-    app.use("/", require("./random.routes"))
-    //app.use("/", require("./api-routes") )
+    app.use("/random", require("./random.routes"))
+    app.use("/api", require("./api-routes") )
 }
