2ecb8bf96553a57ae98bb9e30cb6da05beaf20ed
David Martinez <tinezillo_94@hotmail.com>
2021-09-14 17:31:28 +0200

    Form to create done

--- Project-Pink-Panther/routes/profile.routes.js
+++ Project-Pink-Panther/routes/profile.routes.js
@@ -9,17 +9,18 @@ const User = require("../models/User.model");
 router.get("/", (req, res) => {
   const user = req.session.currentUser;
 
-  User.findById(user.id)
+  User.findById(user._id)
     .then(user => res.render("user/profile", user))
     .catch((err) => console.log(err));
 });
 
 router.post("/", (req, res) => {
+
   const user = req.session.currentUser;
 
   const { email, password, avatarImg } = req.body;
 
-  User.findByIdAndUpdate(user.id, { email, password, avatarImg }, { new: true })
+  User.findByIdAndUpdate(user._id, { email, password, avatarImg })
     .then(() => res.redirect("/"))
     .catch((err) => console.log(err));
 });
